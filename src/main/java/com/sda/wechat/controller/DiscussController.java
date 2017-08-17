091ab0c4858a9f725181b499900cf5793fb4158b
jianlai2007 <jianlai2007@qq.com>
2017-08-17 16:07:33 +0800

    创建企业微信项目

---
+++ src/main/java/com/sda/wechat/controller/DiscussController.java
@@ -0,0 +1,19 @@
+package com.sda.wechat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Allen on 2017/8/11.
+ */
+@Controller
+public class DiscussController {
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+}
